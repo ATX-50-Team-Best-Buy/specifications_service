90fa1ee5bc2cca536965419091d6ab75bf6d1ecf
Mike Champagne <geauxdrum@gmail.com>
2020-08-18 17:35:26 -0500

    installs pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-18-223-97-145.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/front-end-capstone.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:ATX-50-Team-Best-Buy/productInfo-photos_service.git',
+      repo: 'git@github.com:ATX-50-Team-Best-Buy/specifications_service.git',
       path: '/home/ubuntu/champagne_overview',
       'post-deploy': 'npm install && cd client && npm install && npm run build && cd .. && pm2 startOrRestart ecosystem.config.js'
     }
